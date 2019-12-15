470aa173784dc5594ef26ffd54b3551b83982d1b
Mvcampbell3 <mvcampbell3@gmail.com>
2019-12-15 18:52:28 -0500

    express static folder for production

--- server.js
+++ server.js
@@ -7,6 +7,10 @@ const routes = require('./routes');
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
+if (process.env.NODE_ENV === 'production') {
+	app.use(express.static('client/build'));
+}
+
 app.use(routes)
 
 mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost/parksandrec', { useNewUrlParser: true, useUnifiedTopology: true })
