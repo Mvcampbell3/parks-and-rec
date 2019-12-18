eef77c575758c4b06c2ceba9e3c23ac788833b53
Mvcampbell3 <mvcampbell3@gmail.com>
2019-12-18 17:24:36 -0500

    more in-depth seed

--- seeds/index.js
+++ seeds/index.js
@@ -1,7 +1,8 @@
 const db = require('../models');
 const parks = require("./seed");
+const bcrypt = require('bcrypt');
 
-module.exports = function() {
+module.exports.seedParks = function() {
   db.Park.deleteMany()
     .then(() => {
       let promises = []
@@ -14,5 +15,67 @@ module.exports = function() {
         .catch(err => {
           console.log(err)
         })
+    });
+}
+
+module.exports.seedUsers = function() {
+  db.User.deleteMany()
+    .then(result => {
+      console.log(result);
+      // password for user mvcampbell3@gmail.com = test
+      bcrypt.hash('test', 10, function(err, hash) {
+        if (err) throw err;
+        const newUser = new db.User({
+          firstname: 'Michael',
+          lastname: 'Campbell',
+          email: 'mvcampbell3@gmail.com',
+          password: hash
+        })
+
+        newUser.save()
+          .then(user => console.log(user))
+          .catch(err => console.log(err))
+      })
+    })
+    .catch(err => console.log(err))
+}
+
+module.exports.seedTrips = function() {
+
+  let user;
+
+  db.Trip.deleteMany()
+    .then(result => {
+      console.log(result);
+
+      db.User.findOne({ email: 'mvcampbell3@gmail.com' })
+        .then(userDB => {
+          console.log(userDB._id)
+          console.log(userDB)
+          user = userDB._id;
+          db.Park.find()
+            .then(parks => {
+              console.log(user)
+              const newTrip = new db.Trip({
+                
+                userId: user,
+                parkIds: [
+                  parks[40]._id,
+                  parks[12]._id
+                ],
+                name: 'Test Trip'
+              })
+
+              newTrip.save()
+                .then(result => console.log(result))
+                .catch(err => console.log(err))
+            })
+
+        });
+
+
+    })
+    .catch(err => {
+      console.log(err)
     })
 }
--- server.js
+++ server.js
@@ -15,17 +15,35 @@ if (process.env.NODE_ENV === 'production') {
 
 app.use(routes);
 
-const seedDB = false;
+const seedParks = false;
+const seedUsers = false;
+const seedTrips = false;
 const seedRun = require('./seeds');
 
 mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost/parksandrec', { useNewUrlParser: true, useUnifiedTopology: true })
   .then(() => {
     console.log('mongodb connected');
+
+    // Run Parks and/or User seeds first, then run Trips seed with the others set to false
+    // End result of Parks seed is re-seeding all 61 parks
+    // End result of User seed is creating one user Michael Campbell mvcampbell3@gmail.com password=test
+    // End result of Trip seed is creating one trip with test user mvcampbell3@gmail.com to two parks
+
+
+    if (seedParks) {
+      seedRun.seedParks();
+    }
+
+    if (seedUsers) {
+      seedRun.seedUsers();
+    }
+
+    if (seedTrips) {
+      seedRun.seedTrips();
+    }
+
     app.listen(PORT, () => {
       console.log(`server is live on http://localhost:${PORT}`);
-      if (seedDB) {
-        seedRun()
-      }
     })
   })
   .catch(err => {
